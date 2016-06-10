a1ac198a78a84ce092c86e9d9cf6157f0397c4db
Tom <tputs001@gmail.com>
2016-06-10 16:29:59 -0700

    The api call on keypress is now working.

--- public/app.js
+++ public/app.js
@@ -3,13 +3,23 @@ var app = angular.module("slated", ['ngSanitize'])
 app.controller('mainController', function($scope, $http, $sce){
   app.$inject = ['$http', '$sce'];
 
-  $scope.search = function(term){
+  var term = "";
+
+  $scope.keypress = function($event){
+    if($event.keyCode === 8){
+      term = term.slice(0, -1);
+    } else {
+      var input = String.fromCharCode($event.keyCode).toLowerCase()
+      term+= input;
+    }
+    console.log(term)
+
     $http({
       method: "POST",
       url: "/search",
       data: {term: term}
     })
-    .then (function(response){
+    .then(function(response){
       $scope.data = response.data;
     })
   }
